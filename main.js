a8be8e141d126d906aa0a1cedddc2ddb1e860693
dev.fg <eunicelopez_01@hotmail.com>
2023-04-24 19:31:28 -0500

    New changes

---
+++ main.js
@@ -0,0 +1,15 @@
+let numero = 9;
+
+
+/*function suma(numero){
+    console.log(numero + numero)
+    return numero + numero;
+}*/
+
+function suma(numero){
+    let total = numero + numero;
+    console.log(total);
+    return total;
+}
+
+suma(150)
